e3826f6099efaa025edae778db595a04f4a66074
Kagiso Hadebe <60060492+kagiso101@users.noreply.github.com>
2023-11-06 14:06:51 +0200

    [Feature] Create basic layout for app
    - added routing to the app
    - added header , footer and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  loadChildren: () => import('../app/pages/home-page/home-page.module').then(m=>m.HomePageModule),
 
+},
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
